4d060140eb691eae4c2be995c77b0a2d0ceb3101
Trever Hibbs <treverhibbs@gmail.com>
2019-09-20 14:55:39 -0700

    second commit

--- junior/intro_to_software_development/assignment_2/main.js
+++ junior/intro_to_software_development/assignment_2/main.js
@@ -1,13 +1,13 @@
 class MenuItem{
 
-	constructor(name, ingrediance, caloryLowBound, caloryHighBound, category, subcategory, nonDairy) {
+	constructor(name, ingrediance, smallCalorries, regularCalories, category, subcategory, nonDairy) {
 	  this.itemName = name;
 	  this.ingrediance = ingrediance;
-	  this.caloryHighBound = caloryHighBound;
-	  this.caloryLowBound = caloryLowBound;
+	  this.regularCalories = regularCalories;
+	  this.smallCalorries = smallCalorries;
 	  this.category = category;
 	  this.subcategory = subcategory;
-	  this.dairy = dairy;
+	  this.nonDairy = nonDairy;
   }
 }
 
@@ -32,18 +32,14 @@ class Menu{
 }
 
 myBananaberry = new MenuItem("Bananaberry", ["bananas","strawberries"],230,405,"Smoothie", "Classic Favorites", true);
-console.log(myBananaberry.itemName, myBananaberry.ingrediance, myBananaberry.dairy, myBananaberry.caloryLowBound);
+console.log(myBananaberry.itemName, myBananaberry.ingrediance, myBananaberry.nonDairy, myBananaberry.smallCalorries);
 
 myMangoMagic = new MenuItem("Mango Magic", ["mango", "stawberries", "pineapples", "coconut", "creme"], 250, 310, "Smoothie", "Classic Favorites", false);
 
 myGreenSmoothie = new MenuItem("Green Smoothie", ["bananas", "spinach"], 295, 530, "Smoothie", "Healthy & Smart", false);
 
-myPurplePopeye = new MenuItem("Purple Popeye");
+myPurplePopeye = new MenuItem("Purple Popeye", ["spinach", "apple", "beet", "cucumber"], 130, 130, "Juice", "Healthy & Smart", false);
 
-myBrazilianSunrise = new MenuItem("BrazilianSunrise");
+myBrazilianSunrise = new MenuItem("BrazilianSunrise", ["bananas", "acai", "blueberries", "granola"], 515, 780, "Smoothie", "Healthy & Smart", false);
 
-myBeetBooster = new MenuItem("Beet Booster");
-
-myBrazilianSunrise = new MenuItem("BrazilianSunrise");
-
-myBeetBooster = new MenuItem("Beet Booster");
+myBeetBooster = new MenuItem("Beet Booster", ["beet", "orange", "apple"], 225, 225, "Juice", "Healthy & Smart", false);
